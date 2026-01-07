270627272979e614eb50200bd5f59312a9545468
drbiga <mcost16@lsu.edu>
2026-01-07 17:21:49 -0600

    add: converter between light value and brushes for new traffic light version

---
+++ UI/Converters/TrafficLightColorToBrushConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+using LightColor = UI.ViewModels.TrafficLightViewModel.LightColor;
+
+namespace UI.Converters
+{
+    public class TrafficLightColorToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // values[0] = CurrentColor (TrafficLightColor enum)
+            // values[1] = Brush to use if this light is active
+            // values[2] = Brush to use if this light is off
+
+            if (values[0] is not LightColor currentColor) return values[2];
+            if (values[1] is not RadialGradientBrush onBrush) return values[2];
+            if (values[2] is not RadialGradientBrush offBrush) return values[2];
+
+            // parameter = string "Red", "Yellow", "Green" to indicate which light this is
+            var lightName = parameter as string;
+
+            return currentColor switch
+            {
+                LightColor.Red when lightName == "Red" => onBrush,
+                LightColor.Yellow when lightName == "Yellow" => onBrush,
+                LightColor.Green when lightName == "Green" => onBrush,
+                _ => offBrush
+            };
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+
+}
