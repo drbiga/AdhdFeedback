1144b7c610b75d7f7a3af7d6be0a968fa78157bf
drbiga <mcost16@lsu.edu>
2026-01-07 17:23:07 -0600

    add: models folder with existing feedback and iamsession models

---
+++ UI/Models/Feedback.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Services;
+
+namespace UI.Models
+{
+    public class Feedback
+    {
+        public class FeedbackType
+        {
+            public const string FOCUSED = "focused";
+            public const string NORMAL = "normal";
+            public const string DISTRACTED = "distracted";
+        }
+
+        public class PersonalAnalyticsData
+        {
+            public int num_mouse_clicks;
+            public float mouse_move_distance;
+            public float mouse_scroll_distance;
+            public int num_keyboard_strokes;
+            public string attention_feedback;
+        }
+
+        public class ClassifierData
+        {
+            public string screenshot;
+            public string prediction;
+        }
+
+        public PersonalAnalyticsData personal_analytics_data;
+        public ClassifierData classifier_data;
+        public string output;
+    }
+}
---
+++ UI/Models/IamSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Models
+{
+    public class IamSession
+    {
+        public class User
+        {
+            public string username;
+            private string password;
+            public string role;
+        }
+
+        public class Role
+        {
+            static string STUDENT = "student";
+            static string MANAGER = "manager";
+        }
+
+        public string token;
+        public User user;
+        public string ip_address;
+    }
+}
