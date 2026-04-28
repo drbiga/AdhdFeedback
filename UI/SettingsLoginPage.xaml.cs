38eb973fdd56433b2f51c9f5572764a5c8c3285a
drbiga <mcost16@lsu.edu>
2026-04-28 15:37:13 -0500

    add: authorized settings page

---
+++ UI/SettingsLoginPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MessageBox = System.Windows.MessageBox;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for SettingsLoginPage.xaml
+    /// </summary>
+    public partial class SettingsLoginPage : Page
+    {
+        public SettingsLoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Unlock_Click(object sender, RoutedEventArgs e)
+        {
+            // The "Simple/Hardcoded" way
+            if (PassBox.Password == "1234")
+            {
+                // "this.NavigationService" is the magic tool that swaps the page
+                this.NavigationService.Navigate(new SettingsPage());
+            }
+            else
+            {
+                MessageBox.Show("Wrong password!");
+            }
+        }
+    }
+}
---
+++ UI/SettingsPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public List<string> EnvironmentOptionsDisplayNameList { get; set; }
+
+        private Settings _settings;
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+            EnvironmentOptionsDisplayNameList = new List<string>()
+            {
+                Env.Production,
+                Env.Development
+            };
+
+            DataContext = this;
+
+            _settings = Settings.Current;
+            var index = EnvironmentOptionsDisplayNameList.FindIndex(env => env == _settings.Environment);
+            EnvironmentComboBox.SelectedIndex = index; // Default to Production if saved value is invalid
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _settings.Environment = EnvironmentOptionsDisplayNameList[EnvironmentComboBox.SelectedIndex];
+#if DEBUG
+            //System.Windows.MessageBox.Show("Selected environment: " + env);
+#endif
+        }
+    }
+}
